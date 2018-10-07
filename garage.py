a1099d54fab157fb96f543d12cca1fa186bc98b9
Terryqqy <terryqqy@gmail.com>
2018-10-07 10:41:35 -0500

    move car in garage

--- garage.py
+++ garage.py
@@ -10,17 +10,32 @@ the parking lot from the initial state to the final state.
 a[x:y:z] means : x is the former xth data is removed, y is after yth data is removed , so [x to y] is keeped, z is the stride it is going to take
 """
 
-def garage(ini,fin):
-    ini = ini[::]
+def garage(initial,fin):
+    ini = initial[::] #make a copy to prevent the change to original one
     steps = 0
     seq = []#out put of the sequence of each step
+    """
+    so in this loop we would do two path:
+    check first data is same or not then if not move the first one to 
+    the place in final. If yes then go to check next one.
+    """
     while ini != fin:
-    zero = ini.index(0)
-    if zero != fin.index(0):
-	car_moves = fin[zero]
-	pos = ini.index(car_moves)
-	ini[zero],ini[pos] = ini[pos], ini[zero]
-    else:
-	for i in range(len(ini)):
-	    if ini[i] != fin[i]:
-		ini[zero]
+	zero = ini.index(0)#that is the pin point
+	if zero != fin.index(0):
+	    pos = ini.index(fin[zero])#python could return the place of zero element
+	    ini[zero],ini[pos] = ini[pos], ini[zero]#swap them!  so aewsome
+	else:
+	    for i in range(len(ini)):
+		if ini[i] != fin[i]:
+		    ini[zero], ini[i] = ini[i], ini[zero]
+		    break
+	seq.append(ini[::])#careful with the indent!
+	steps +=1
+    return steps, seq
+
+a = [1,2,3,0,4]
+b = [0,3,2,1,4]
+opta, optb = garage(a,b)
+print 'the steps are', opta,' the sequence is ', optb
+
+	    
