40f03cbffa9df8f402be2731487f804ac9a123e7
TERRYQQY <terryqqy@gmail.com>
2018-10-11 14:04:20 -0500

    dependent
    dependent

--- circle.py
+++ circle.py
@@ -5,7 +5,7 @@ the next counter starts immediately after the member is removed.
 Print till all the members are exhausted.
 For example:
 Input: consider 123456789 members sitting in a circular fashion,
-Output: 369485271.
+Output: 369485271.d
 """
 def circle(ipt):
     opt = ipt[::]
