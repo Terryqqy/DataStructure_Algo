163af76852dde20e0756b799fc523aea202c0168
Terryqqy <terryqqy@gmail.com>
2018-10-22 23:57:29 -0500

    temp

--- merge_interval.py
+++ merge_interval.py
@@ -9,7 +9,7 @@ Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 Example 2:
 
 Input: [[1,4],[4,5]]
-Output: [[1,5]]
+Output: [[1,5]].
 Explanation: Intervals [1,4] and [4,5] are considerred overlapping.
 """
 
