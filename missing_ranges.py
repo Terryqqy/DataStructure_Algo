9bc0b6684b8e662ea316f4da1c65b9b441df4e07
Terryqqy <terryqqy@gmail.com>
2018-10-22 23:54:22 -0500

    temp2

--- missing_ranges.py
+++ missing_ranges.py
@@ -3,5 +3,11 @@ Find missing ranges between low and high in the given array.
 Ex) [3, 5] lo=1 hi=10 => answer: [(1, 2), (4, 4), (6, 10)]
 """
 def missing_ranges(ipt,lowvalue, highvalue):
-    
+    out = []
+    for i in range(len(ipt)):
+        if i != len(ipt)-1:
+            if ipt[i] > lowvalue:
+                out.append((lowvalue, ipt[i]-1))
+
+                
 
