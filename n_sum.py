ba72d016a9ab7724355357503ab91201f2581ab2
TERRYQQY <terryqqy@gmail.com>
2018-10-25 22:50:10 -0500

    temp

---
+++ n_sum.py
@@ -0,0 +1,26 @@
+"""
+Given an array of n integers, are there elements a, b, .. , n in nums
+such that a + b + .. + n = target?
+Find all unique n-tuplets in the array which gives the sum of target.
+Example:
+    basic:
+        Given:
+            n = 4
+            nums = [1, 0, -1, 0, -2, 2]
+            target = 0,
+        return [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+"""
+def n_sum(ipt, resultnumber, target):
+    """
+    Get the total number of combination of the possible results.
+    """
+    lenofipt = len(ipt)
+    times= helper = lenofipt
+    for i in range(resultnumber):
+        times*=(helper-1)
+
+    """
+    To iterate all possible result and store in the dict
+    """
+    a = [[]]
+    
