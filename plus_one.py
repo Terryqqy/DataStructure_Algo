a2325eddc417e586ff373272eb52ef4261ad319d
Terryqqy <terryqqy@gmail.com>
2018-10-30 23:52:00 -0500

    temp

---
+++ plus_one.py
@@ -0,0 +1,16 @@
+"""
+Given a non-negative number represented as an array of digits,
+adding one to each numeral.
+The digits are stored big-endian, such that the most significant
+digit is at the head of the list.
+Eg:
+[1,2,4] -- [1,2,5]
+[1,9] -- [2,0]
+[9,9,9] -- [1,0,0,0]
+so it is just use array to represent numbers and do plus
+"""
+
+def plus_one(ipt):
+    if ipt[-1] == 9:
+    
+
