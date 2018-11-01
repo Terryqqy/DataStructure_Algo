db14e650a2d3aad416290db65fc14ad2c2421b98
Terryqqy <terryqqy@gmail.com>
2018-11-01 07:46:02 -0500

    temp

---
+++ summarize_ranges.py
@@ -0,0 +1,10 @@
+
+"""
+Given a sorted integer array without duplicates,
+return the summary of its ranges.
+For example, given [0, 1, 2, 4, 5, 7], return [(0, 2), (4, 5), (7, 7)].
+"""
+
+def sum_range(ipt):
+    for i in ipt:
+        
