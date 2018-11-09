148a083f71ad33c1dd0a0e9933440dcde4a48359
TERRYQQY <terryqqy@gmail.com>
2018-11-09 10:31:47 -0600

    independent

--- top_1.py
+++ top_1.py
@@ -5,7 +5,27 @@ so this function returns a list. This result can be used to find a
 representative value in an array.
 This algorithm gets an array, makes a dictionary of it,
  finds the most frequent count, and makes the result list.
- For example: top_1([1, 1, 2, 2, 3, 4]) will return [1, 2]
- (TL:DR) Get mathematical Mode
- Complexity: O(n)
- """
+For example: top_1([1, 1, 2, 2, 3, 4]) will return [1, 2]
+(TL:DR) Get mathematical Mode
+Complexity: O(n)
+"""
+def top_1(ipt): 
+    topnum = 0
+    dictforelem = {}
+    output = []
+    for i in ipt:
+        if i in dictforelem:
+            dictforelem[i] += 1
+        else:
+            dictforelem[i] = 1
+    for i in dictforelem:
+        if dictforelem[i] == topnum:
+            output.append(i)
+        elif dictforelem[i] > topnum:
+            output[:] = []
+            output.append(i)
+            topnum = dictforelem[i]
+    return output
+
+a = [1,1,2,2,3,4,2,3,3]
+print(top_1(a))
